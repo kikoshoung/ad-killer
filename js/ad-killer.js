230e810d7ebb4a74b4b0cff5bd2541096b92b138
kikoshoung <kiko.xiang@guohead.com>
2013-06-01 00:51:29 +0800

    modi

--- js/ad-killer.js
+++ js/ad-killer.js
@@ -77,11 +77,10 @@
 
 	// kill this ad element. If it has a wrapper, kill its wrapper too
 	var killSuspectableDoms = function(child){
-		console.log(child)
 		var parent = child.parentNode,
 			suspectableAttr = getSuspectableAttr(parent);
 		parent.removeChild(child);
-		if(suspectableAttr.match(regexCompany)) parent.parentNode.removeChild(parent);
+		if(suspectableAttr.match(regexCompany) && parent.parentNode) parent.parentNode.removeChild(parent);
 	}
 
 	// kill action start from here. Function 'excu' is the only method that export for 'window.adKillerByKikoshoung'
